3cea0d0babc0adb3a790932f2c72494c24f534bf
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2018-12-27 06:21:23 +0100

    KI-4 Implement an agent that almost always eats all gold in unknown world with passive wumpus and pits.

--- WumpusAgent/src/de/fh/MyAgent.java
+++ WumpusAgent/src/de/fh/MyAgent.java
@@ -1,18 +1,7 @@
 package de.fh;
 
-import java.util.LinkedList;
 import de.fh.agent.WumpusHunterAgent;
-import de.fh.blanks.CellInfo;
-import de.fh.blanks.CellType;
 import de.fh.blanks.HunterWorld;
-import de.fh.blanks.Point;
-import de.fh.heuristicalSearch.AStern;
-import de.fh.heuristicalSearch.Bestensuche;
-import de.fh.heuristicalSearch.Dijkstra;
-import de.fh.suche.Suche;
-import de.fh.suche.Suche.Suchstrategie;
-import de.fh.uninformedSearch.Breitensuche;
-import de.fh.uninformedSearch.Tiefensuche;
 import de.fh.wumpus.HunterPercept;
 import de.fh.wumpus.enums.HunterAction;
 import de.fh.wumpus.enums.HunterActionEffect;
@@ -25,10 +14,7 @@ import de.fh.wumpus.enums.HunterActionEffect;
  */
 public class MyAgent extends WumpusHunterAgent {
 
-	HunterWorld world = new HunterWorld();
-	Suchstrategie suchstrategie = Suchstrategie.BREITENSUCHE;
-	LinkedList<HunterAction> hunterActionList = new LinkedList<HunterAction>();
-	boolean moveHunter = true;
+	HunterWorld hunterWorld = new HunterWorld();
 	
 	public static void main(String[] args) {
 
@@ -54,20 +40,21 @@ public class MyAgent extends WumpusHunterAgent {
 	 */
 	@Override
 	public void updateState(HunterPercept percept, HunterActionEffect actionEffect) {
-		this.world.updateState(percept, actionEffect);
+		this.hunterWorld.updateHunterPosition(nextAction);
+		this.hunterWorld.updateState(percept, actionEffect);
 	}
 
 	/**
-	 * Diesen Part erweitern Sie so, dass die nÃ¤chste(n) sinnvolle(n) Aktion(en),
-	 * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen, ausgefÃ¼hrt wird/werden.
-	 * Der action-Part soll den Agenten so intelligent wie mÃ¶glich handeln lassen
+	 * Diesen Part erweitern Sie so, dass die nächste(n) sinnvolle(n) Aktion(en),
+	 * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen, ausgeführt wird/werden.
+	 * Der action-Part soll den Agenten so intelligent wie möglich handeln lassen
 	 *
 	 * Beispiel: Wenn die letzte Wahrnehmung
 	 * "percept.isGlitter() == true" enthielt, ist "HunterAction.GRAB" eine
 	 * geeignete Tätigkeit. Wenn Sie wissen, dass ein Quadrat "unsicher"
 	 * ist, können Sie wegziehen
 	 *
-	 * @return Die nÃ¤chste HunterAction die vom Server ausgefÃ¼hrt werden soll
+	 * @return Die nächste HunterAction die vom Server ausgeführt werden soll
 	 */
 	@Override
 	public HunterAction action() {
@@ -84,65 +71,8 @@ public class MyAgent extends WumpusHunterAgent {
 		HunterAction.QUIT_GAME
 		*/
 		
+		nextAction = this.hunterWorld.getNextAction();
 		
-
-//		this.world.processAction(nextAction);
-//		nextAction = HunterAction.GO_FORWARD;
-//		System.out.println(world);
-		
-		Suche suche = null;
-		Point zielPosition = new Point(4, 4);
-		if (this.moveHunter) {
-			
-			nextAction = HunterAction.SIT;
-			// Test
-			// Virtuelle Welt aufbauen
-			for (int i = 1; i < 4; i++) {
-				world.set(i, 1, new CellInfo(CellType.BREEZE));
-			}
-			world.set(3, 2, new CellInfo(CellType.BREEZE));
-			world.set(3, 3, new CellInfo(CellType.BREEZE));
-			
-			world.set(2, 3, new CellInfo(CellType.BREEZE));
-			world.set(2, 3, new CellInfo(CellType.STENCH));
-			
-			world.set(2, 4, new CellInfo(CellType.BREEZE));
-			world.set(3, 4, new CellInfo(CellType.BREEZE));
-			world.set(4, 4, new CellInfo(CellType.BREEZE));
-			// end Virtuelle Welt aufbauen
-			world.print();
-            switch (suchstrategie){
-                case TIEFENSUCHE:
-                    suche = new Tiefensuche(this.world, zielPosition);
-                    break;
-                case BREITENSUCHE:
-                    suche = new Breitensuche(this.world, zielPosition);
-                    break;
-                case DIJKSTAR:
-                    suche = new Dijkstra(this.world, zielPosition);
-                    break;
-                case BESTENSUCHE:
-                    suche = new Bestensuche(this.world, zielPosition);
-                    break;
-                case ASTERN:
-                    suche = new AStern(this.world, zielPosition);
-                    break;
-            }
-//            
-    		hunterActionList = suche.start();
-            
-            moveHunter = false;
-		} else {
-    		if(hunterActionList != null) {
-    			if(!hunterActionList.isEmpty()) {
-    				nextAction = hunterActionList.remove();
-    				System.out.println("-> " + nextAction.toString());
-    			} else {
-    				nextAction = HunterAction.QUIT_GAME;
-    			}
-    		}
-		}
-	
 		return nextAction;
 	}
 }
