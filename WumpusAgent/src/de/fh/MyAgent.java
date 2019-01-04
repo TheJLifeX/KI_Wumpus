108b11fc9aa54cd0677b231699d7a4d479bae3ca
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2019-01-04 10:44:41 +0100

    Fix some bug.

--- WumpusAgent/src/de/fh/MyAgent.java
+++ WumpusAgent/src/de/fh/MyAgent.java
@@ -14,15 +14,14 @@ public class MyAgent extends WumpusHunterAgent {
 
 	HunterWorld hunterWorld = new HunterWorld();
 	
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 
 		MyAgent agent = new MyAgent("");
 		MyAgent.start(agent,"127.0.0.1", 5000);
 	}
 
-	public MyAgent(String name) 
-	{
+	public MyAgent(String name) {
+
 		super(name);
 	}
 
@@ -38,45 +37,39 @@ public class MyAgent extends WumpusHunterAgent {
 	 * @param actionEffect Reaktion des Servers auf vorhergewÃ¤hlte Aktion
 	 */
 	@Override
-	public void updateState(HunterPercept percept, HunterActionEffect actionEffect) 
-	{
-		System.out.println("--nextAction in updateState: " + nextAction + "--");
-
-		// nextAction ist hier die previousAction da diese zum Zeitpunkt des Aufrufs von updateState(...) schon
-		// ausgefuehrt wurde
-		hunterWorld.updateState(percept, actionEffect, nextAction);
+	public void updateState(HunterPercept percept, HunterActionEffect actionEffect) {
+		this.hunterWorld.updateState(percept, actionEffect, nextAction);
 	}
 
 	/**
 	 * Diesen Part erweitern Sie so, dass die nächste(n) sinnvolle(n) Aktion(en),
-	 * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen, ausgeführt wird/werden.
-	 * Der action-Part soll den Agenten so intelligent wie möglich handeln lassen
+	 * auf Basis der vorhandenen Zustandsinformationen und gegebenen Zielen,
+	 * ausgeführt wird/werden. Der action-Part soll den Agenten so intelligent wie
+	 * möglich handeln lassen
 	 *
-	 * Beispiel: Wenn die letzte Wahrnehmung
-	 * "percept.isGlitter() == true" enthielt, ist "HunterAction.GRAB" eine
-	 * geeignete Tätigkeit. Wenn Sie wissen, dass ein Quadrat "unsicher"
-	 * ist, können Sie wegziehen
+	 * Beispiel: Wenn die letzte Wahrnehmung "percept.isGlitter() == true" enthielt,
+	 * ist "HunterAction.GRAB" eine geeignete Tätigkeit. Wenn Sie wissen, dass ein
+	 * Quadrat "unsicher" ist, können Sie wegziehen
 	 *
 	 * @return Die nächste HunterAction die vom Server ausgeführt werden soll
 	 */
 	@Override
-	public HunterAction action() 
-	{
+	public HunterAction action() {
 
-		/*HunterAction
-        Mögliche HunterActions sind möglich:
+		/*
+		 * HunterAction Mögliche HunterActions sind möglich:
+		 * 
+		 * HunterAction.GO_FORWARD 
+		 * HunterAction.TURN_LEFT 
+		 * HunterAction.TURN_RIGHT
+		 * HunterAction.SHOOT 
+		 * HunterAction.SIT 
+		 * HunterAction.GRAB 
+		 * HunterAction.QUIT_GAME
+		 */
 
-       	HunterAction.GO_FORWARD
-       	HunterAction.TURN_LEFT
-		HunterAction.TURN_RIGHT
-		HunterAction.SHOOT
-		HunterAction.SIT
-		HunterAction.GRAB
-		HunterAction.QUIT_GAME
-		*/
-		
-		nextAction = hunterWorld.action();
-		
+		nextAction = this.hunterWorld.getNextAction();
+
 		return nextAction;
 	}
 }
